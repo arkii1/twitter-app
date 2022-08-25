d1b3ff64f0827024db57cf3f9585cff0b99dedbf
arkii1 <tommy.mapp@hotmail.com>
2022-08-25 11:03:13 +0100

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react"
 import "./App.css"
+import "./index.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-import SignIn from "./components/SignIn/SignIn"
+import SignIn from "./components/SignIn"
+import SignUp from "./components/SignUp"
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <BrowserRouter>
+        <Routes>
+          <Route path="sign-in" element={<SignIn />} />
+          <Route path="sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
