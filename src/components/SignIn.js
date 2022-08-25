b198e0c6f45cf07a93194570085c7de7153a3923
arkii1 <tommy.mapp@hotmail.com>
2022-08-25 11:04:29 +0100

    Add non-funciotnal SignIn modal

---
+++ src/components/SignIn.js
@@ -0,0 +1,38 @@
+import React from "react"
+import "./SignIn.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faXmark } from "@fortawesome/free-solid-svg-icons"
+import LabelAndInput from "./LabelAndInput"
+import googleLogo from "../assets/google-logo.svg"
+import twitterAnim from "../assets/twitter-anim.gif"
+import Button from "./Button"
+
+function SignIn() {
+  return (
+    <div className="sign-in d-flex flex-column justify-content-start align-items-center bg-white gap-4 p-5">
+      <FontAwesomeIcon icon={faXmark} className="sign-in__x p-2" />
+      <img src={twitterAnim} alt="" style={{ height: "3rem", top: "1rem" }} />
+      <h1 style={{ height: "1.8rem" }} className="mb-5">
+        Sign in to Twitter
+      </h1>
+      <div className="d-flex flex-column justify-content-center align-items-center gap-2 w-100">
+        <Button logo={googleLogo} text="Continue with Google" colours="light" />
+        or
+        <form
+          className="sign-in__form d-flex flex-column justify-content-center align-items-center gap-2 w-100"
+          method="POST"
+          action=""
+        >
+          <LabelAndInput labelText="Email" type="email" forName="email" />
+          <Button text="Next" colours="dark" />
+        </form>
+        <Button text="Forgotten password?" colours="light" />
+      </div>
+      <span className="sign-in__no-account my-3">
+        Don&apos;t have an account? <a href="/sign-up">Sign up</a>
+      </span>
+    </div>
+  )
+}
+
+export default SignIn
