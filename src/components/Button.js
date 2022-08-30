4f21318013e2da8110bb413cabc3c5cb46803af2
arkii1 <tommy.mapp@hotmail.com>
2022-08-30 10:44:39 +0100

    Modify relative width

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ function Button({ logo, text, colours, onClick }) {
   return (
     <button
       type="button"
-      className={`button--${colours} d-flex gap-1 align-items-center justify-content-center w-75`}
+      className={`button--${colours} d-flex gap-1 align-items-center justify-content-center w-100`}
       onClick={onClick}
     >
       {logo && <img src={logo} alt="" style={{ height: "1rem" }} />}
