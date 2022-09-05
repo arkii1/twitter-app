ddbffa2226ac6cb7f569344824d694e9f1887ea6
arkii1 <tommy.mapp@hotmail.com>
2022-09-05 14:44:01 +0100

    Add Protected component

---
+++ src/components/Protected.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Navigate } from "react-router-dom"
+import propTypes from "prop-types"
+import { useAuth } from "../contexts/AuthContext"
+
+function Protected({ children }) {
+  const { currentUser } = useAuth()
+  if (!currentUser) {
+    return <Navigate to="/modal/sign-in" />
+  }
+
+  return children
+}
+
+export default Protected
+
+Protected.propTypes = {
+  children: propTypes.object,
+}
