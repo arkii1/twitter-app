eb0d353cdb3d149313e04fd732e71bc7b0f3923d
arkii1 <tommy.mapp@hotmail.com>
2022-09-05 10:54:20 +0100

    Add AuthProvder plus cleanup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { AuthProvider } from "./contexts/AuthContext"
 import "./App.css"
 import "./index.css"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
-import SignIn from "./components/SignIn"
-import SignUp from "./components/SignUp"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-100 h-100">
       <BrowserRouter>
-        <Routes>
-          <Route path="sign-in" element={<SignIn />} />
-          <Route path="sign-up" element={<SignUp />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="sign-in" element={<SignIn />} />
+            <Route path="sign-up" element={<SignUp />} />
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </div>
   )
