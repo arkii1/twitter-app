f3805d70893b6872f73c12b757038d34421dbd36
arkii1 <tommy.mapp@hotmail.com>
2022-09-05 11:46:49 +0100

    Style SignIn

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,6 @@
 import React from "react"
 import "./SignIn.css"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faXmark } from "@fortawesome/free-solid-svg-icons"
+import { Link } from "react-router-dom"
 import LabelAndInput from "../components/LabelAndInput"
 import googleLogo from "../assets/google-logo.svg"
 import twitterAnim from "../assets/twitter-anim.gif"
@@ -20,32 +19,31 @@ function SignIn() {
   }
 
   return (
-    <div className="sign-in d-flex flex-column justify-content-start align-items-center bg-white gap-4 p-5">
-      <FontAwesomeIcon icon={faXmark} className="sign-in__x p-2" />
+    <div className="sign-in w-75 h-100 d-flex flex-column align-items-center justify-content-between">
       <img src={twitterAnim} alt="" style={{ height: "3rem", top: "1rem" }} />
-      <h1 style={{ height: "1.8rem" }} className="mb-5">
-        Sign in to Twitter
-      </h1>
-      <div className="d-flex flex-column justify-content-center align-items-center gap-2 w-100">
-        <Button
-          logo={googleLogo}
-          text="Continue with Google"
-          colours="light"
-          onClick={handelGoogleSignIn}
+      <h1>Sign in to Twitter</h1>
+      <Button
+        logo={googleLogo}
+        text="Continue with Google"
+        colours="light"
+        onClick={handelGoogleSignIn}
+      />
+      or
+      <form className="w-100 d-flex flex-column gap-2">
+        <LabelAndInput labelText="Email" type="email" forName="email" />
+        <LabelAndInput
+          labelText="Password"
+          type="password"
+          forName="password"
         />
-        or
-        <form
-          className="sign-in__form d-flex flex-column justify-content-center align-items-center gap-2 w-100"
-          method="POST"
-          action=""
-        >
-          <LabelAndInput labelText="Email" type="email" forName="email" />
-          <Button text="Next" colours="dark" />
-        </form>
-        <Button text="Forgotten password?" colours="light" />
-      </div>
-      <span className="sign-in__no-account my-3">
-        Don&apos;t have an account? <a href="/sign-up">Sign up</a>
+        <Button text="Sign In" colours="dark" />
+      </form>
+      <Button text="Forgotten password?" colours="light" />
+      <span className="small my-3">
+        Don&apos;t have an account?{" "}
+        <Link className="react-link" to="/modal/sign-up">
+          Sign up
+        </Link>
       </span>
     </div>
   )
