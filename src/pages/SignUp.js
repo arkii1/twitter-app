faab78fc7b55735204f4ff11a0fbcc3d115c594b
arkii1 <tommy.mapp@hotmail.com>
2022-09-05 15:24:47 +0100

    Add redirect when signing in

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react"
 import "./SignUp.css"
-import { Link } from "react-router-dom"
+import { Link, Navigate } from "react-router-dom"
 import twitterAnim from "../assets/twitter-anim.gif"
 import Button from "../components/Button"
 import LabelAndInput from "../components/LabelAndInput"
@@ -10,7 +10,7 @@ function SignUp() {
   const emailRef = useRef()
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
-  const { signup } = useAuth()
+  const { signup, currentUser } = useAuth()
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
 
@@ -33,6 +33,7 @@ function SignUp() {
     return setLoading(false)
   }
 
+  if (currentUser) return <Navigate to="/account" />
   return (
     <div className="sign-up w-75 h-100 d-flex flex-column align-items-center justify-content-between">
       <img src={twitterAnim} alt="" style={{ height: "3rem", top: "1rem" }} />
