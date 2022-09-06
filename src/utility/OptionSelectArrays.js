512c4df6f06b006d930967f7bd76d2ca015431bc
arkii1 <tommy.mapp@hotmail.com>
2022-09-06 16:04:53 +0100

    Add utility function to get options array for birtdate

---
+++ src/utility/OptionSelectArrays.js
@@ -0,0 +1,31 @@
+export function getMonths() {
+  return [
+    "",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Augest",
+    "September",
+    "October",
+    "November",
+    "December",
+  ]
+}
+
+export function getDays() {
+  const arr = [...Array(31).keys()].map((n) => n.toString())
+  arr.unshift("")
+  return arr
+}
+
+export function getYears() {
+  const arr = [...Array(100).keys()].map((n) =>
+    (new Date().getFullYear() - n).toString()
+  )
+  arr.unshift("")
+  return arr
+}
