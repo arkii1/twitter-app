1c3ce6b1e7c0ae6fff398cbbce3b5fb3c83e011b
arkii1 <tommy.mapp@hotmail.com>
2022-09-06 16:05:38 +0100

    Add ability to pass children as a prop, and add overflow styling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,22 @@
 import React from "react"
 import "./Modal.css"
 import { Outlet } from "react-router-dom"
+import propTypes from "prop-types"
 
-function Modal() {
+function Modal({ children }) {
   return (
-    <div className="rounded-modal p-3 d-flex flex-column justify-content-start align-items-center">
-      <Outlet />
+    <div
+      className="rounded rounded-modal d-flex flex-column justify-content-start align-items-center"
+      style={{ overflowY: "scroll", overflowX: "hidden" }}
+    >
+      {!children && <Outlet />}
+      {children && children}
     </div>
   )
 }
 
 export default Modal
+
+Modal.propTypes = {
+  children: propTypes.any,
+}
