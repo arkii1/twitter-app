7ec436fba8bc4968f85218f9ead8b7a23a06f199
arkii1 <tommy.mapp@hotmail.com>
2022-09-08 10:43:56 +0100

    Add firestoreUtils

---
+++ src/utility/firestoreUtils.js
@@ -0,0 +1,71 @@
+import {
+  collection,
+  query,
+  where,
+  doc,
+  setDoc,
+  getDocs,
+} from "firebase/firestore"
+import { firestore } from "../firebase"
+
+const db = firestore
+
+export async function getUserDetailsFromAuthID(id) {
+  const userDetailsRef = collection(db, "userDetails")
+  const q = await query(userDetailsRef, where("userID", "==", id))
+  const queryResult = await getDocs(q)
+  const qArr = []
+  queryResult.forEach((d) => {
+    qArr.push(d.data())
+  })
+
+  if (qArr > 1)
+    return console.log(
+      "Two documents in userDetails have the same userID. id: ",
+      qArr[0].userID
+    )
+  if (qArr[0]) {
+    return qArr[0]
+  }
+  return {
+    avatarURL: "",
+    bgURL: "",
+    bio: "",
+    birthDay: "",
+    birthMonth: "",
+    birthYear: "",
+    location: "",
+    name: "",
+    userID: "",
+    username: "",
+    website: "",
+  }
+}
+
+export function getUserDetailsFromEmail() {}
+
+export function getMainTweets() {}
+
+export async function createOrUpdateUserDetails(email, details) {
+  const docRef = doc(db, "userDetails", email)
+  try {
+    await setDoc(docRef, details)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export async function usernameAlreadyExists(username, id) {
+  const userDetailsRef = collection(db, "userDetails")
+  const q = await query(
+    userDetailsRef,
+    where("username", "==", username),
+    where("userID", "!=", id)
+  )
+  const queryResult = await getDocs(q)
+  const qArr = []
+  queryResult.forEach((d) => {
+    qArr.push(d.data())
+  })
+  return qArr.length > 0
+}
