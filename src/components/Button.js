c2616bd24e89243d01001b2ef9d93659b606708c
arkii1 <tommy.mapp@hotmail.com>
2022-09-08 14:25:45 +0100

    Add option to have font awesome icons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,16 @@
 import React from "react"
 import "./Button.css"
 import propTypes from "prop-types"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-function Button({ logo, text, colours, onClick, type = "button" }) {
+function Button({
+  faLogo,
+  logo,
+  text,
+  colours = "light",
+  onClick,
+  type = "button",
+}) {
   return (
     <button
       // eslint-disable-next-line react/button-has-type
@@ -11,6 +19,7 @@ function Button({ logo, text, colours, onClick, type = "button" }) {
       onClick={onClick}
     >
       {logo && <img src={logo} alt="" style={{ height: "1rem" }} />}
+      {faLogo && <FontAwesomeIcon icon={faLogo} />}
       {text}
     </button>
   )
@@ -20,8 +29,9 @@ export default Button
 
 Button.propTypes = {
   logo: propTypes.node,
-  text: propTypes.string.isRequired,
-  colours: propTypes.string.isRequired,
-  onClick: propTypes.func, // make this required later,
+  text: propTypes.string,
+  colours: propTypes.string,
+  onClick: propTypes.func,
   type: propTypes.string,
+  faLogo: propTypes.string,
 }
