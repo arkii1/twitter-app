a37c607445de2c8cf7e01484051e3b12d379dd14
arkii1 <tommy.mapp@hotmail.com>
2022-09-13 15:21:16 +0100

    Add manual Logout

---
+++ src/components/Logout.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react"
+import { Link } from "react-router-dom"
+import { useAuth } from "../contexts/AuthContext"
+
+function Logout() {
+  const { logout } = useAuth()
+
+  useEffect(() => {
+    logout()
+  }, [])
+
+  return (
+    <>
+      <h1>Logged out. Sign in:</h1>{" "}
+      <Link to="/modal/sign-in">
+        <button type="button">Sign in</button>
+      </Link>
+    </>
+  )
+}
+
+export default Logout
