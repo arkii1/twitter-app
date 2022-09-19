b21ab5399ba23c3921ce13729ae628592554c450
arkii1 <tommy.mapp@hotmail.com>
2022-09-19 11:43:05 +0100

    Add LinkTabs and Outlet

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,7 +6,7 @@ import {
   faLocationDot,
   faLink,
 } from "@fortawesome/free-solid-svg-icons"
-import { Link, useParams } from "react-router-dom"
+import { Link, useParams, Outlet } from "react-router-dom"
 import { useDetails } from "../contexts/UserDetailsContext"
 import ImageContainer from "../components/ImageContainer"
 import Button from "../components/Button"
@@ -18,6 +18,7 @@ import {
   isFollowing,
 } from "../utility/firestoreUtils"
 import FollowingFollowersLinks from "../components/FollowingFollowersLinks"
+import LinkTabs from "../components/LinkTabs"
 
 function Profile() {
   const { userDetails } = useDetails()
@@ -56,73 +57,83 @@ function Profile() {
     setFollowing(false)
   }
 
+  const linkTabsData = [
+    {
+      link: `/app/${id}/tweets/tweets`,
+      text: "Tweets",
+    },
+    {
+      link: `/app/${id}/tweets/tweets&replies`,
+      text: "Tweets & replies",
+    },
+    {
+      link: `/app/${id}/tweets/media`,
+      text: "Media",
+    },
+    {
+      link: `/app/${id}/tweets/likes`,
+      text: "Likes",
+    },
+  ]
+
   return (
     !loading && (
       <>
         {configureProfile && isUsersPage && (
           <ConfigureProfile exit={() => setConfigureProfile(false)} />
         )}
-        <div className="w-100 h-100">
+        <div className="w-100">
           <ImageContainer src={details.bgURL} alt="" type="bg" />
           <span
-            className="p-2"
-            style={{
-              width: "max-content",
-              float: "right",
-            }}
+            className="d-flex justify-content-between align-items-start w-100 p-2 pb-0"
+            style={{ maxHeight: "5rem" }}
           >
-            {" "}
-            {isUsersPage && (
-              <Button
-                text="Edit profile"
-                onClick={() => setConfigureProfile(true)}
-              />
-            )}
-            {!isUsersPage &&
-              (following ? (
-                <Button text="Unfollow" onClick={handleUnfollow} />
-              ) : (
-                <Button text="Follow" colours="dark" onClick={handleFollow} />
-              ))}
-          </span>
-          <span className="d-flex ">
-            <span
-              style={{
-                position: "absolute",
-                transform: "translate(0.5rem, -4rem)",
-                background: "rgba(0,0,0,0)",
-              }}
-              className="d-flex flex-column gap-3"
-            >
+            <span style={{ transform: "translateY(-55%)" }}>
               <ImageContainer src={details.avatarURL} alt="" type="avatar" />
-              <span className="d-flex flex-column gap-1">
-                <h3 className="profile__h3">{details.name}</h3>
-                <h4 className="small">@{details.username}</h4>
-              </span>
-              {details.bio !== "" && (
-                <p className="profile__p">{details.bio}</p>
+            </span>
+            <span>
+              {isUsersPage && (
+                <Button
+                  text="Edit profile"
+                  onClick={() => setConfigureProfile(true)}
+                />
               )}
-              <span className="d-flex gap-4 justify-content-start align-items-center small">
-                {details.location !== "" && (
-                  <span>
-                    <FontAwesomeIcon icon={faLocationDot} /> {details.location}
-                  </span>
-                )}
-                {details.website !== "" && (
-                  <span>
-                    <Link className="react-link small" to={details.website}>
-                      <FontAwesomeIcon icon={faLink} /> {details.website}
-                    </Link>
-                  </span>
-                )}
-                <span>
-                  <FontAwesomeIcon icon={faCalendar} /> Joined{" "}
-                  {details.createdAt.month} {details.createdAt.year}
-                </span>
-              </span>
-              <FollowingFollowersLinks details={details} />
+              {!isUsersPage &&
+                (following ? (
+                  <Button text="Unfollow" onClick={handleUnfollow} />
+                ) : (
+                  <Button text="Follow" colours="dark" onClick={handleFollow} />
+                ))}
             </span>
           </span>
+          <span className="d-flex flex-column gap-3 w-50 px-2 pb-2">
+            <span className="d-flex flex-column gap-1">
+              <h3 className="profile__h3">{details.name}</h3>
+              <h4 className="small">@{details.username}</h4>
+            </span>
+            {details.bio !== "" && <p className="profile__p">{details.bio}</p>}
+            <span className="d-flex gap-4 justify-content-start align-items-center small">
+              {details.location !== "" && (
+                <span>
+                  <FontAwesomeIcon icon={faLocationDot} /> {details.location}
+                </span>
+              )}
+              {details.website !== "" && (
+                <span>
+                  <Link className="react-link small" to={details.website}>
+                    <FontAwesomeIcon icon={faLink} /> {details.website}
+                  </Link>
+                </span>
+              )}
+              <span>
+                <FontAwesomeIcon icon={faCalendar} /> Joined{" "}
+                {details.createdAt.month} {details.createdAt.year}
+              </span>
+            </span>
+            <FollowingFollowersLinks details={details} />
+          </span>
+          <LinkTabs links={linkTabsData} />
+          <Outlet />
         </div>
       </>
     )
