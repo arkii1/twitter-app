26e9cd9ac79093a2c48ddd398132e084dc13235c
arkii1 <tommy.mapp@hotmail.com>
2022-09-19 11:42:34 +0100

    Modify links

--- src/components/FollowingFollowersLinks.js
+++ src/components/FollowingFollowersLinks.js
@@ -5,13 +5,19 @@ import { Link } from "react-router-dom"
 function FollowingFollowersLinks({ details }) {
   return (
     <span className="d-flex gap-3 justify-content-start align-items-center small">
-      <Link className="react-link" to={`/app/${details.username}/following`}>
+      <Link
+        className="react-link"
+        to={`/app/${details.username}/community/following`}
+      >
         <span className="small">
           <span className="small--bold">{details.following.length}</span>{" "}
           Following
         </span>
       </Link>
-      <Link className="react-link" to={`/app/${details.username}/followers`}>
+      <Link
+        className="react-link"
+        to={`/app/${details.username}/community/followers`}
+      >
         <span className="small">
           <span className="small--bold">{details.followers.length}</span>{" "}
           Followers
