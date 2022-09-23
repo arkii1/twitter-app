f43be4b0b59fba414c3245f5fdaabb17101a35fb
arkii1 <tommy.mapp@hotmail.com>
2022-09-23 10:17:41 +0100

    Add gray-on-hover option

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -2,12 +2,12 @@ import React from "react"
 import "./ImageContainer.css"
 import propTypes from "prop-types"
 
-function ImageContainer({ src, alt, type }) {
+function ImageContainer({ src, alt, type, hover = false }) {
   return (
     <div
       className={`image-container--${type} ${
         type === "avatar" || type === "avatar--small" ? "rounded-circle" : ""
-      }`}
+      } ${hover ? "gray-img-on-hover" : ""}`}
     >
       <img
         className={`h-100 image-container__img--${type} ${
@@ -26,4 +26,5 @@ ImageContainer.propTypes = {
   src: propTypes.string.isRequired,
   alt: propTypes.string.isRequired,
   type: propTypes.string.isRequired,
+  hover: propTypes.bool,
 }
