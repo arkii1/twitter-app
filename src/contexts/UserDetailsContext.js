ccab0ea8bd02b10fa65efd2236e8b1d0f35f6251
arkii1 <tommy.mapp@hotmail.com>
2022-09-25 14:49:11 +0100

    Modify for new firestore functions

--- src/contexts/UserDetailsContext.js
+++ src/contexts/UserDetailsContext.js
@@ -1,16 +1,11 @@
-import React, {
-    useCallback,
-    useContext,
-    useEffect,
-    useMemo,
-    useState,
-} from 'react'
+import React, { useCallback, useContext, useMemo, useState } from 'react'
 
 import propTypes from 'prop-types'
 
 import {
-    createOrUpdateUserDetails,
-    getUserDetailsFromID,
+    createUserDetails,
+    getUserDetails,
+    updateUserDetails,
 } from '../utility/firestore/userDetailsFirestore'
 import { useAuth } from './AuthContext'
 
@@ -24,38 +19,25 @@ export function UserDetailsProvider({ children }) {
     const { currentUser } = useAuth()
 
     const [userDetails, setUserDetails] = useState()
-    const [loading, setLoading] = useState(true)
-
-    const updateUserDetails = useCallback(
+    const updateUserDetailsContext = useCallback(
         async (details) => {
-            await createOrUpdateUserDetails(currentUser.email, details)
-            const updatedDetails = await getUserDetailsFromID(currentUser.uid)
+            if (userDetails) {
+                await updateUserDetails(currentUser.uid, details)
+            } else await createUserDetails(currentUser.uid, details)
+            const updatedDetails = await getUserDetails(currentUser.uid)
             setUserDetails(updatedDetails)
         },
-        [currentUser],
+        [currentUser, userDetails],
     )
 
     const value = useMemo(
-        () => ({ userDetails, updateUserDetails }),
-        [userDetails, updateUserDetails],
+        () => ({ userDetails, updateUserDetailsContext }),
+        [userDetails, updateUserDetailsContext],
     )
-
-    useEffect(() => {
-        const initCurDetails = async () => {
-            if (currentUser) {
-                setLoading(true)
-                const details = await getUserDetailsFromID(currentUser.uid)
-                if (details) setUserDetails(details)
-                setLoading(false)
-            }
-        }
-
-        initCurDetails()
-    }, [currentUser])
 
     return (
         <UserDetailsContext.Provider value={value}>
-            {!loading && children}
+            {children}
         </UserDetailsContext.Provider>
     )
 }
