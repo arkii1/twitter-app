082783df5114f2a0c9162c7b0bbfcc691c1c3a3d
arkii1 <tommy.mapp@hotmail.com>
2022-10-17 16:05:35 +0100

    Add retweetedBy functionality

--- src/components/common/TweetCard.js
+++ src/components/common/TweetCard.js
@@ -30,13 +30,14 @@ function TweetCard({ tweet }) {
     const imageRef = useRef()
 
     const [tweetDetails, setTweetDetails] = useState(tweet.tweetData)
-    const [tweetUserDetails, setTweetUserDetails] = useState(null)
-    const [profileModal, setProfileModal] = useState(false)
-    const [modalPos, setModalPos] = useState([0, 0])
-    const [liked, setLiked] = useState(null)
     const [retweetedByUser, setRetweetedByUser] = useState(
         tweet.tweetData.retweets.indexOf(userDetails.id) !== -1,
     )
+    const [tweetUserDetails, setTweetUserDetails] = useState(null)
+    const [retweetUserDetails, setRetweetUserDetails] = useState()
+    const [profileModal, setProfileModal] = useState(false)
+    const [modalPos, setModalPos] = useState([0, 0])
+    const [liked, setLiked] = useState(null)
 
     const handleModalOn = (ref) => {
         setProfileModal(true)
@@ -80,6 +81,10 @@ function TweetCard({ tweet }) {
         const init = async () => {
             const details = await getUserDetails(tweetDetails.userID)
             setTweetUserDetails(details)
+            if (tweet.retweetedUser && !retweetedByUser) {
+                const d = await getUserDetails(tweet.retweetedUser)
+                setRetweetUserDetails(d)
+            }
             const like = userDetails.likedTweets.indexOf(tweetDetails.id) !== -1
             setLiked(like)
             if (imageRef.current) {
@@ -104,117 +109,132 @@ function TweetCard({ tweet }) {
                         />
                     </span>
                 )}
-                <div
-                    className="tweet-card d-flex justify-content-start align-items-start w-100 gap-2 p-2"
-                    onMouseLeave={handleModalOff}
-                >
-                    <Link
-                        to={`/app/${tweetUserDetails.username}`}
-                        className="react-link--none"
-                        onMouseEnter={() => handleModalOn(imageRef)}
-                        ref={imageRef}
-                    >
-                        <ImageContainer
-                            src={tweetUserDetails.avatarURL}
-                            alt=""
-                            type="avatar--small"
-                            hover
-                        />
-                    </Link>
-                    <div className="d-flex flex-column justify-content-start align-items-start w-100 gap-1">
-                        <div className="d-flex w-100 justify-content-between align-items-center">
+                <div className="d-flex flex-column justify-content-start align-items-start">
+                    {retweetUserDetails && (
+                        <span className="small" style={{ marginLeft: '4rem' }}>
+                            Retweeted by{' '}
                             <Link
-                                className="react-link--none d-flex gap-1 align-items-end justify-content-start"
-                                to={`/app/${tweetUserDetails.username}`}
-                                ref={nameRef}
-                                onMouseEnter={() => handleModalOn(nameRef)}
+                                to={`/app/${retweetUserDetails.username}`}
+                                className="react-link--none"
                             >
-                                <span className="name underline-on-hover">
-                                    {tweetUserDetails.name}
-                                </span>
-                                <span className="username">
-                                    @{tweetUserDetails.username}
-                                </span>
+                                {retweetUserDetails.username}
                             </Link>
-                            <span className="tweet-card__ellipsis">
-                                <Button
-                                    faLogo={faEllipsis}
-                                    colours="inherit"
-                                    size="0.75rem"
-                                />
-                            </span>
-                        </div>
-                        <span className="text mb-1">{tweetDetails.text}</span>
-                        <div className="d-flex justify-content-between w-75">
-                            <span className="tweet-card__comment d-flex justify-content-start align-items-center gap-1">
-                                <Button
-                                    faLogo={faComment}
-                                    size="1.1rem"
-                                    padding="2"
-                                    colours="inherit"
-                                />
-                                <span
-                                    style={{
-                                        color: 'inherit',
-                                        backgroundColor: 'transparent',
-                                        fontSize: '0.9rem',
-                                    }}
+                        </span>
+                    )}
+                    <div
+                        className="tweet-card d-flex justify-content-start align-items-start w-100 gap-2 p-2"
+                        onMouseLeave={handleModalOff}
+                    >
+                        <Link
+                            to={`/app/${tweetUserDetails.username}`}
+                            className="react-link--none"
+                            onMouseEnter={() => handleModalOn(imageRef)}
+                            ref={imageRef}
+                        >
+                            <ImageContainer
+                                src={tweetUserDetails.avatarURL}
+                                alt=""
+                                type="avatar--small"
+                                hover
+                            />
+                        </Link>
+                        <div className="d-flex flex-column justify-content-start align-items-start w-100 gap-1">
+                            <div className="d-flex w-100 justify-content-between align-items-center">
+                                <Link
+                                    className="react-link--none d-flex gap-1 align-items-end justify-content-start"
+                                    to={`/app/${tweetUserDetails.username}`}
+                                    ref={nameRef}
+                                    onMouseEnter={() => handleModalOn(nameRef)}
                                 >
-                                    {tweetDetails.replies.length}
+                                    <span className="name underline-on-hover">
+                                        {tweetUserDetails.name}
+                                    </span>
+                                    <span className="username">
+                                        @{tweetUserDetails.username}
+                                    </span>
+                                </Link>
+                                <span className="tweet-card__ellipsis">
+                                    <Button
+                                        faLogo={faEllipsis}
+                                        colours="inherit"
+                                        size="0.75rem"
+                                    />
                                 </span>
+                            </div>
+                            <span className="text mb-1">
+                                {tweetDetails.text}
                             </span>
-                            <span
-                                className={`tweet-card__retweet${
-                                    retweetedByUser ? '--retweeted' : ''
-                                } d-flex justify-content-start align-items-center gap-1`}
-                            >
-                                <Button
-                                    faLogo={faRetweet}
-                                    size="1.1rem"
-                                    padding="2"
-                                    colours="inherit"
-                                    onClick={handleRetweet}
-                                />
+                            <div className="d-flex justify-content-between w-75">
+                                <span className="tweet-card__comment d-flex justify-content-start align-items-center gap-1">
+                                    <Button
+                                        faLogo={faComment}
+                                        size="1.1rem"
+                                        padding="2"
+                                        colours="inherit"
+                                    />
+                                    <span
+                                        style={{
+                                            color: 'inherit',
+                                            backgroundColor: 'transparent',
+                                            fontSize: '0.9rem',
+                                        }}
+                                    >
+                                        {tweetDetails.replies.length}
+                                    </span>
+                                </span>
                                 <span
-                                    style={{
-                                        color: 'inherit',
-                                        backgroundColor: 'transparent',
-                                        fontSize: '0.9rem',
-                                    }}
+                                    className={`tweet-card__retweet${
+                                        retweetedByUser ? '--retweeted' : ''
+                                    } d-flex justify-content-start align-items-center gap-1`}
                                 >
-                                    {tweetDetails.retweets.length}{' '}
+                                    <Button
+                                        faLogo={faRetweet}
+                                        size="1.1rem"
+                                        padding="2"
+                                        colours="inherit"
+                                        onClick={handleRetweet}
+                                    />
+                                    <span
+                                        style={{
+                                            color: 'inherit',
+                                            backgroundColor: 'transparent',
+                                            fontSize: '0.9rem',
+                                        }}
+                                    >
+                                        {tweetDetails.retweets.length}{' '}
+                                    </span>
                                 </span>
-                            </span>
-                            <span
-                                className={`tweet-card__like--${
-                                    liked ? 'liked' : 'unliked'
-                                } d-flex justify-content-start align-items-center gap-1`}
-                            >
-                                <Button
-                                    faLogo={faHeart}
-                                    size="1.1rem"
-                                    padding="2"
-                                    colours="inherit"
-                                    onClick={handleLikeTweet}
-                                />
                                 <span
-                                    style={{
-                                        color: 'inherit',
-                                        backgroundColor: 'transparent',
-                                        fontSize: '0.9rem',
-                                    }}
+                                    className={`tweet-card__like--${
+                                        liked ? 'liked' : 'unliked'
+                                    } d-flex justify-content-start align-items-center gap-1`}
                                 >
-                                    {tweetDetails.likes.length}
+                                    <Button
+                                        faLogo={faHeart}
+                                        size="1.1rem"
+                                        padding="2"
+                                        colours="inherit"
+                                        onClick={handleLikeTweet}
+                                    />
+                                    <span
+                                        style={{
+                                            color: 'inherit',
+                                            backgroundColor: 'transparent',
+                                            fontSize: '0.9rem',
+                                        }}
+                                    >
+                                        {tweetDetails.likes.length}
+                                    </span>
                                 </span>
-                            </span>
-                            <span className="tweet-card__share">
-                                <Button
-                                    faLogo={faArrowUpFromBracket}
-                                    size="1.1rem"
-                                    padding="2"
-                                    colours="inherit"
-                                />
-                            </span>
+                                <span className="tweet-card__share">
+                                    <Button
+                                        faLogo={faArrowUpFromBracket}
+                                        size="1.1rem"
+                                        padding="2"
+                                        colours="inherit"
+                                    />
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
